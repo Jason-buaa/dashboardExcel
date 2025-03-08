d5b22cf3a133d3eb20b578746085c3b7e9e2e814
Jason_buaa <waterworth@qq.com>
2025-03-08 10:10:15 +0800

    demenstrate gruop and move group function

--- src/taskpane/taskpane.ts
+++ src/taskpane/taskpane.ts
@@ -10,6 +10,8 @@ Office.onReady((info) => {
     document.getElementById("sideload-msg").style.display = "none";
     document.getElementById("app-body").style.display = "flex";
     document.getElementById("run").onclick = run;
+    document.getElementById("gruop").onclick = group_shape;
+    document.getElementById("move").onclick = group_move;
   }
 });
 
@@ -29,6 +31,35 @@ export async function run() {
       textbox.height = 20;
       textbox.width = 45;
       textbox.name = "Textbox";
+      await context.sync();
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function group_shape() {
+  try {
+    await Excel.run(async (context) => {
+      let shapes = context.workbook.worksheets.getItem("Sheet1").shapes;
+      let square = shapes.getItem("Square");
+      let textbox = shapes.getItem("Textbox");
+      let shapeGroup = shapes.addGroup([square, textbox]);
+      shapeGroup.name = "Group";
+      console.log("Shapes grouped");
+      await context.sync();
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function group_move() {
+  try {
+    await Excel.run(async (context) => {
+      let shapes = context.workbook.worksheets.getItem("Sheet1").shapes;
+      let shapeGroup = shapes.getItem("Group");
+      shapeGroup.incrementLeft(50);
       await context.sync();
     });
   } catch (error) {
